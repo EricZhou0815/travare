675f0c2287f59fc73744a46fe91798c98e002f87
Junchao Zhong <jczhong115@gmail.com>
2019-01-12 14:16:21 +1300

    - fix loading page layout and theme issue.

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:travare/loading_screen.dart';
-import 'package:travare/main_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: MaterialApp(
-        title: 'Travare',
-        theme: ThemeData.light(),
-        initialRoute: "/",
-        routes: {
-          "/": (context) => LoadingPage(),
-          "/main": (context) => MainPage(),
-        },
+    return MaterialApp(
+      title: 'Travare',
+      theme: ThemeData(
+
       ),
+      home: LoadingPage(),
     );
   }
 }
-
